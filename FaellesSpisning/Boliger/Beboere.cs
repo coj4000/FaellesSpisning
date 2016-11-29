7c6f1b54268599674c57909d3bed93cdaadd6525
Christian <Christian@WORKSTATION>
2016-11-29 12:36:34 +0100

    lavet beboere

---
+++ FaellesSpisning/Boliger/Beboere.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaellesSpisning.Boliger
+{
+    class Beboere
+    {
+        public Beboere()
+        {
+            
+        }
+
+        public void Voksne(string navn, int antal)
+        {
+            
+        }
+        public void Unge(int antal)
+        { }
+        public void Børn(int antal)
+        { }
+        public void Børnu3(int antal)
+        { }
+    }
+}

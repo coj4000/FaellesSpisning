e1b5f89329dd69d68740ac68ebe2efd603763195
Christian <Christian@WORKSTATION>
2016-12-01 11:35:26 +0100

    Its Changes

---
+++ FaellesSpisning/Planlægning/Skema.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaellesSpisning.Planlægning
+{
+    class Skema
+    {
+        public Skema()
+        {
+            
+        }
+
+        public void Add()
+        {
+            
+        }
+
+        public void Delete()
+        {
+            
+        }
+    }
+}

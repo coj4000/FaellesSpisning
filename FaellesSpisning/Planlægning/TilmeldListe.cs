e266d1eb6b162ca382a59f22dd30758f33d2738d
Matias <Matias@DESKTOP-TA0TGEP>
2016-12-07 10:43:59 +0100

    Indsatte metoder og defualt konstructor i tilmeldteBoliger

--- FaellesSpisning/Planlægning/TilmeldListe.cs
+++ FaellesSpisning/Planlægning/TilmeldListe.cs
@@ -7,26 +7,7 @@ using System.Threading.Tasks;
 namespace FaellesSpisning.Planlægning
 {
     class TilmeldListe
-    {
-        public int AntalBørn(int antal)
-        {
-            return antal;
-        }
-
-        public int AntalBørnU3(int antal)
-        {
-            return antal;
-        }
-
-        public int AntalUnge(int antal)
-        {
-            return antal;
-        }
-
-        public int AntalVoksne(int antal)
-        {
-            return antal;
-        }
+    {       
         public List<TilmeldteBoliger> Tilmeldinger()
         {
             
--- FaellesSpisning/Planlægning/TilmeldteBoliger.cs
+++ FaellesSpisning/Planlægning/TilmeldteBoliger.cs
@@ -8,5 +8,44 @@ namespace FaellesSpisning.Planlægning
 {
     class TilmeldteBoliger
     {
+        public TilmeldteBoliger()
+        {
+
+        }
+    
+        public double AntalBørn(int antal)
+        {
+            double result = antal * 0.25;
+            return result;
+        }
+
+        public int AntalBørnU3(int antal)
+        {
+            int result = antal * 0;
+            return result;
+        }
+
+        public double AntalUnge(int antal)
+        {
+            double result = antal * 0.5;
+            return result;
+        }
+
+        public double AntalVoksne(int antal)
+        {
+            double result = antal * 1;
+            return result;
+        }
+
+        public double AntalKuverter(int Børn, int BørnU3, int Unge, int Voksne)
+        {
+            double Kuverter = AntalBørnU3(BørnU3);
+            Kuverter += AntalBørn(Børn);
+            Kuverter += AntalUnge(Unge);
+            Kuverter += AntalVoksne(Voksne);
+            return Kuverter;
+        }
+
+        
     }
 }

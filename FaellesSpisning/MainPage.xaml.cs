3d33b528f0a114144e8dbf7af9ae943647ede0bf
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2016-12-19 11:07:49 +0100

    Jonatan did stuff

--- FaellesSpisning/MainPage.xaml.cs
+++ FaellesSpisning/MainPage.xaml.cs
@@ -26,18 +26,78 @@ namespace FaellesSpisning
         public MainPage()
         {
             this.InitializeComponent();
+            comboBox1.Items.Add("bolignr = 1, BørnU3 = 1, Børn = 0, Unge = 2, Voksne = 2");
+            comboBox1.Items.Add("bolignr = 2, BørnU3 = 0, Børn = 3, Unge = 0, Voksne = 2");
+            comboBox1.Items.Add("bolignr = 3, BørnU3 = 0, Børn = 1, Unge = 1, Voksne = 3");
+            comboBox1.Items.Add("bolignr = 4, BørnU3 = 1, Børn = 0, Unge = 2, Voksne = 2");
+            comboBox1.Items.Add("bolignr = 5, BørnU3 = 0, Børn = 1, Unge = 1, Voksne = 2");
+            comboBox1.Items.Add("bolignr = 6, BørnU3 = 0, Børn = 2, Unge = 0, Voksne = 1");
+            comboBox1.Items.Add("bolignr = 7, BørnU3 = 3, Børn = 1, Unge = 0, Voksne = 3");
+            comboBox1.Items.Add("bolignr = 8, BørnU3 = 2, Børn = 1, Unge = 1, Voksne = 2");
+            comboBox1.Items.Add("bolignr = 9, BørnU3 = 0, Børn = 3, Unge = 1, Voksne = 1");
+            comboBox1.Items.Add("bolignr = 10, BørnU3 = 0, Børn = 0, Unge = 3, Voksne = 2");
 
 
+        }
+
+        private void Tilføj_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                textBox.Text += "bolignr = 1, BørnU3 = 1, Børn = 0, Unge = 2, Voksne = 2" + Environment.NewLine;
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                textBox.Text += "bolignr = 2, BørnU3 = 0, Børn = 3, Unge = 0, Voksne = 2" + Environment.NewLine;
+            }
+            if (comboBox1.SelectedIndex == 2)
+            {
+                textBox.Text += "bolignr = 3, BørnU3 = 0, Børn = 1, Unge = 1, Voksne = 3" + Environment.NewLine;
+            }
+            if (comboBox1.SelectedIndex == 3)
+            {
+                textBox.Text += "bolignr = 4, BørnU3 = 1, Børn = 0, Unge = 2, Voksne = 2" + Environment.NewLine;
+            }
+            if (comboBox1.SelectedIndex == 4)
+            {
+                textBox.Text += "bolignr = 5, BørnU3 = 0, Børn = 1, Unge = 1, Voksne = 2" + Environment.NewLine;
+            }
+            if (comboBox1.SelectedIndex == 5)
+            {
+                textBox.Text += "bolignr = 6, BørnU3 = 0, Børn = 2, Unge = 0, Voksne = 1" + Environment.NewLine;
+            }
+            if (comboBox1.SelectedIndex == 6)
+            {
+                textBox.Text += "bolignr = 7, BørnU3 = 3, Børn = 1, Unge = 0, Voksne = 3" + Environment.NewLine;
+            }
+            if (comboBox1.SelectedIndex == 7)
+            {
+                textBox.Text += "bolignr = 8, BørnU3 = 2, Børn = 1, Unge = 1, Voksne = 2" + Environment.NewLine;
+            }
+            if (comboBox1.SelectedIndex == 8)
+            {
+                textBox.Text += "bolignr = 9, BørnU3 = 0, Børn = 3, Unge = 1, Voksne = 1" + Environment.NewLine;
+            }
+            if (comboBox1.SelectedIndex == 9)
+            {
+                textBox.Text += "bolignr = 10, BørnU3 = 0, Børn = 0, Unge = 3, Voksne = 2" + Environment.NewLine;
+            }
         }
 
 
         private void Buttonredirect_OnClick(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(Udregn), listView.SelectedItem);
+            //this.Frame.Navigate(typeof(Udregn), listView.SelectedItem);
         }
 
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
 
+        }
 
-    
+        private void Tilføj_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "";
+        }
     }
 }
